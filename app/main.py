4d1cafff606f0767b5e1937d886868efe69bceb3
vn3 <vinicosta.vn3dev@gmail.com>
2026-02-01 16:03:59 -0300

    add comments to main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
+# server wen flask para exibir um html com o host e headers que o nginx injeta
+
 from flask import Flask, request
 import os
 
 app = Flask(__name__)
 
-DEFAULT_COLOR = os.getenv("COLOR", "#222")
-DEFAULT_LABEL = os.getenv("CLIENT_LABEL", "cliente")
+DEFAULT_COLOR = os.getenv("COLOR", "#222") # tenta ler color, se não encontrar usa #222
+DEFAULT_LABEL = os.getenv("CLIENT_LABEL", "cliente") # mesma coisa mas com o nome do cliente
 
-@app.get("/")
+@app.get("/") 
 def home():
-    host = request.headers.get("Host", "")
-    subdomain = host.split(".")[0] if "." in host else host
+    host = request.headers.get("Host", "") # pega o header http do host, se n existir faz uma string vazia
+    subdomain = host.split(".")[0] if "." in host else host # se o host tiver ".", ele divide a string e pega só a primeira parte antes do ponto
 
+# pega label e color da requisição e se n tiver, usa default
     client_label = request.headers.get("X-Client-Label", DEFAULT_LABEL)
     color = request.headers.get("X-Color", DEFAULT_COLOR)
 
+# estrutura html para exibir os dados
     return f"""
     <html>
       <body style="margin:0; font-family: Arial; background:{color}; color:white;">
